82b3ba34a080d02729752bbaf40d7e9c10898ae6
profxaker <profxaker@mail.ru>
2015-12-05 14:19:29 +0200

    Add script

---
+++ mail.class.php
@@ -0,0 +1,38 @@
+﻿<?php
+class MailSend {
+	private $email; // Адресс
+	private $subject; // Тема
+	private $text; // Текст сообщения
+	
+	// Добавляем адресс
+	public function addEmail($email) {
+		$this->email = $email;
+	}
+	
+	// Добавляем тему
+	public function addSubject($subject) {
+		$this->subject = $subject;
+	}
+	
+	// Добавляем текст
+	public function addText($text) {
+		$this->text = $text;
+	}
+	
+	// Отправка
+	public function send() {
+		mail($this->email, $this->subject, $this->text);
+	}
+	
+	// Валидация входящих данных
+	private function validate() {
+		
+	}
+}
+
+$mail = new MailSend();
+$mail->addEmail('profxaker@mail.ru');
+$mail->addSubject('Title message');
+$mail->addText('Text message');
+$mail->send();
+?>
